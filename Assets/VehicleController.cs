7dd5e58b92c827d09a17b69c936d973055aa8c42
Tom Hyde <thyde@neworbit.co.uk>
2020-01-24 23:14:48 +0000

    Physics tweaking

--- Assets/VehicleController.cs
+++ Assets/VehicleController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Linq;
 using UnityEngine;
 
 public class VehicleController : MonoBehaviour
@@ -7,16 +6,19 @@ public class VehicleController : MonoBehaviour
     public GameObject FrontWheels;
     public GameObject BackWheels;
     public float Acceleration = 1;
+    public float Drag = 1;
     public float Steering = 1;
     public float Grip = 1;
-    private Rigidbody rigidBody;
+    public float NudgeAwayStrength = 1;
+    private Camera myCamera;
     private float acceleratorValue;
     private float steeringValue;
+    private Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.rigidBody = this.GetComponent<Rigidbody>();
+        this.myCamera = this.GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
@@ -32,8 +34,46 @@ public class VehicleController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        this.rigidBody.AddForceAtPosition(this.transform.forward * this.acceleratorValue * this.Acceleration, this.FrontWheels.transform.position, ForceMode.Force);
-        this.rigidBody.AddForce(this.transform.right * - this.transform.InverseTransformVector(this.rigidBody.velocity).x * this.Grip);
-        this.transform.RotateAround(this.BackWheels.transform.position, this.transform.up, this.steeringValue * this.Steering * this.transform.InverseTransformVector(this.rigidBody.velocity).z);
+        var velocityDegradation = -this.velocity.normalized * Drag * Time.deltaTime;
+        var degradedVelocity = this.velocity + velocityDegradation;
+        if (Vector3.Angle(this.velocity, degradedVelocity) > 90)
+        {
+            // "Drag" only slows, not reverses
+            degradedVelocity = Vector3.zero;
+        }
+
+        this.velocity = degradedVelocity + acceleratorValue * this.Acceleration * Time.deltaTime * this.transform.forward;
+        this.transform.Rotate(this.transform.up, this.steeringValue * this.Steering);
+        var rotationOverlaps = Physics.OverlapBox(this.transform.position, this.transform.lossyScale / 2, this.transform.rotation)
+            .Where(collider => collider is BoxCollider && !collider.gameObject.transform.IsChildOf(this.transform));
+        if (rotationOverlaps.Any())
+        {
+            var collider = rotationOverlaps.First();
+            var hitPoint = collider.ClosestPoint(this.transform.position);
+            this.velocity = this.velocity + (this.transform.position - hitPoint).normalized * this.NudgeAwayStrength;
+            this.transform.Translate(this.velocity, Space.World);
+            return;
+        }
+
+        var castHits = Physics.BoxCastAll(this.transform.position, this.transform.lossyScale / 2, this.velocity.normalized, this.transform.rotation, this.velocity.magnitude)
+            .Where(hit => !hit.collider.gameObject.transform.IsChildOf(this.transform));
+        if (castHits.Any())
+        {
+            this.Bounce(castHits.First().normal);
+            return;
+        }
+
+        this.transform.Translate(this.velocity, Space.World);
+    }
+
+    private void LateUpdate()
+    {
+        this.myCamera.transform.SetPositionAndRotation(this.myCamera.transform.position, Quaternion.Euler(90, 0, 0));
+    }
+
+    private void Bounce(Vector3 normal)
+    {
+        var updatedVelocity = this.velocity - 2 * normal * Vector3.Dot(this.velocity, normal);
+        this.velocity = new Vector3(updatedVelocity.x, 0, updatedVelocity.z);
     }
 }
