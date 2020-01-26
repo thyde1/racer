26519992901bebaa090bcaa7ac33f0079554a255
Tom Hyde <thyde@neworbit.co.uk>
2020-01-26 21:42:43 +0000

    Camera scaled (badly) for multiplayer

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -20,5 +20,15 @@ public class CameraController : MonoBehaviour
         var positions2D = targets.Select(t => Vector3Utils.Make2D(t.transform.position));
         var centrePosition = new Vector3(new[] { positions2D.Min(v => v.x), positions2D.Max(v => v.x) }.Average(), 0, new[] { positions2D.Min(v => v.z), positions2D.Max(v => v.z) }.Average());
         this.myCamera.transform.SetPositionAndRotation(centrePosition + new Vector3(0, 80, -30), Quaternion.Euler(70, 0, 0));
+        while (!AreAllTargetsVisible())
+        {
+            this.myCamera.transform.SetPositionAndRotation(centrePosition + new Vector3(0, this.myCamera.transform.position.y + 1, -30), Quaternion.Euler(70, 0, 0));
+        }
+    }
+
+    private bool AreAllTargetsVisible()
+    {
+        var viewpointPositions = targets.Select(t => this.myCamera.WorldToViewportPoint(t.transform.position));
+        return !viewpointPositions.Any(p => p.x < 0.1 || p.x > 0.9 || p.y < 0.1 || p.y > 0.9);
     }
 }
