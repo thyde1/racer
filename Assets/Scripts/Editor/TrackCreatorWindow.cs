2612b99df8143b26ac33ce24831949beac9af820
Tom Hyde <thyde@neworbit.co.uk>
2020-02-01 13:58:54 +0000

    Refactor DoOverArc

--- Assets/Scripts/Editor/TrackCreatorWindow.cs
+++ Assets/Scripts/Editor/TrackCreatorWindow.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -32,30 +34,41 @@ public class TrackCreatorWindow : EditorWindow
     public static void CreateTrack(float width, float angle)
     {
         var track = new GameObject("Track");
+        var innerWall = CreateCubeArc(track.transform, 1, angle * Mathf.Deg2Rad);
+        var outerWall = CreateCubeArc(track.transform, width +1, angle* Mathf.Deg2Rad);
+
         var trackGround = new GameObject("Ground", typeof(MeshRenderer), typeof(MeshFilter));
         trackGround.transform.SetParent(track.transform);
         var groundMeshFilter = trackGround.GetComponent<MeshFilter>();
-        groundMeshFilter.mesh = new Mesh();
-        CreateCubeArc(track.transform, width + 1, angle * Mathf.Deg2Rad);
-        CreateCubeArc(track.transform, 1, angle * Mathf.Deg2Rad);
+        var groundMesh = groundMeshFilter.sharedMesh = new Mesh();
+        groundMesh.vertices = innerWall.transform.GetComponentsInChildren<MeshFilter>().SelectMany(filter => filter.mesh.vertices).ToArray();
     }
 
-    private static void CreateCubeArc(Transform parent, float radius, float angleInRadians)
+    private static void DoOverArc(float radius, float angleInRadians, Action<Vector3, float, float> action)
     {
         var desiredSegmentLength = 0.4f;
         var segments = Mathf.Ceil(angleInRadians / desiredSegmentLength);
         var segmentLength = angleInRadians / segments;
+        for (var t = segmentLength * 0.5f; t < angleInRadians; t += segmentLength)
+        {
+            action(new Vector3(radius * Mathf.Cos(t), 0, radius * Mathf.Sin(t)), t, segmentLength);
+        }
+    }
 
+    private static GameObject CreateCubeArc(Transform parent, float radius, float angleInRadians)
+    {
         var arc = new GameObject("Arc");
         arc.transform.parent = parent;
 
-        for (var t = segmentLength * 0.5f; t < angleInRadians; t+= segmentLength)
+        DoOverArc(radius, angleInRadians, (position, t, segmentLength) =>
         {
             var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube.transform.SetParent(arc.transform);
             cube.transform.localScale = new Vector3(1, 1, (radius + 0.6f) * segmentLength);
-            cube.transform.position = new Vector3(radius * Mathf.Cos(t), 0, radius * Mathf.Sin(t));
+            cube.transform.position = position;
             cube.transform.rotation = Quaternion.Euler(0, -t * Mathf.Rad2Deg + 180, 0);
-        }
+        });
+
+        return arc;
     }
 }
