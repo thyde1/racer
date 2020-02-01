8bd8a247630a0b993ecba117cd919e4332aa8c19
Tom Hyde <thyde@neworbit.co.uk>
2020-02-01 16:51:30 +0000

    Configurable wall thickness

--- Assets/Scripts/Editor/TrackCreatorWindow.cs
+++ Assets/Scripts/Editor/TrackCreatorWindow.cs
@@ -7,9 +7,9 @@ using UnityEngine;
 
 public class TrackCreatorWindow : EditorWindow
 {
-    private float bendWidth = 10f;
+    private float trackWidth = 10f;
     private float angle = 90f;
-    private float straightWidth;
+    private float wallThickness = 1f;
 
     [MenuItem("Window/Track Creator")]
     public static void OpenWindow()
@@ -21,26 +21,26 @@ public class TrackCreatorWindow : EditorWindow
     private void OnGUI()
     {
         this.titleContent = new GUIContent("Track Creator");
-        GUILayout.Label("Bend");
+        GUILayout.Label("Wall Thickness");
+        float.TryParse(GUILayout.TextField(this.wallThickness.ToString()), out this.wallThickness);
         GUILayout.Label("Width");
-        float.TryParse(GUILayout.TextField(this.bendWidth.ToString()), out this.bendWidth);
+        float.TryParse(GUILayout.TextField(this.trackWidth.ToString()), out this.trackWidth);
+        GUILayout.Label("Bend");
         GUILayout.Label("Angle");
         float.TryParse(GUILayout.TextField(this.angle.ToString()), out this.angle);
         if (GUILayout.Button("Generate Bend"))
         {
-            CreateBend(this.bendWidth, this.angle);
+            CreateBend(this.trackWidth, this.angle);
         };
 
         GUILayout.Label("Straight");
-        GUILayout.Label("Width");
-        float.TryParse(GUILayout.TextField(this.bendWidth.ToString()), out this.straightWidth);
         if (GUILayout.Button("Generate Straight"))
         {
-            CreateStraight(this.straightWidth);
+            CreateStraight(this.trackWidth);
         };
     }
 
-    public static void CreateBend(float width, float angle)
+    public void CreateBend(float width, float angle)
     {
         var bend = new GameObject("Bend");
         var innerWall = CreateCubeArc(bend.transform, 1, angle * Mathf.Deg2Rad);
@@ -50,26 +50,28 @@ public class TrackCreatorWindow : EditorWindow
         var outerVertices = DoOverArc(width, angle * Mathf.Deg2Rad, false, (pos, t, segmentLength) => pos);
         GenerateGround(innerVertices, outerVertices, bend.transform);
 
+        bend.transform.SetPositionAndRotation(Selection.activeTransform.position, Selection.activeTransform.rotation);
         Selection.activeGameObject = bend;
     }
 
-    public static void CreateStraight(float width)
+    public void CreateStraight(float width)
     {
         var straight = new GameObject("Straight");
         var innerWall = GenerateWall(straight.transform);
         innerWall.transform.position = new Vector3(1, 0, 0);
-        innerWall.transform.localScale = new Vector3(1, 1, width - 1);
+        innerWall.transform.localScale = new Vector3(this.wallThickness, 1, width - 1);
         var outerWall = GenerateWall(straight.transform);
         outerWall.transform.position = new Vector3(width, 0, 0);
-        outerWall.transform.localScale = new Vector3(1, 1, width - 1);
+        outerWall.transform.localScale = new Vector3(this.wallThickness, 1, width - 1);
         var innerVertices = new[] { innerWall.transform.position - (width - 1) * 0.5f * innerWall.transform.forward, innerWall.transform.position + (width - 1) * 0.5f * innerWall.transform.forward };
         var outerVertices = new[] { outerWall.transform.position - (width - 1) * 0.5f * outerWall.transform.forward, outerWall.transform.position + (width - 1) * 0.5f * outerWall.transform.forward };
         GenerateGround(innerVertices, outerVertices, straight.transform);
 
+        straight.transform.SetPositionAndRotation(Selection.activeTransform.position, Selection.activeTransform.rotation);
         Selection.activeGameObject = straight;
     }
 
-    private static IEnumerable<T> DoOverArc<T>(float radius, float angleInRadians, bool doAtCentres, Func<Vector3, float, float, T> action)
+    private IEnumerable<T> DoOverArc<T>(float radius, float angleInRadians, bool doAtCentres, Func<Vector3, float, float, T> action)
     {
         var desiredSegmentLength = 0.2f;
         var segments = Mathf.Ceil(angleInRadians / desiredSegmentLength);
@@ -83,7 +85,7 @@ public class TrackCreatorWindow : EditorWindow
         return results;
     }
 
-    private static GameObject CreateCubeArc(Transform parent, float radius, float angleInRadians)
+    private GameObject CreateCubeArc(Transform parent, float radius, float angleInRadians)
     {
         var arc = new GameObject("Arc");
         arc.transform.parent = parent;
@@ -91,7 +93,7 @@ public class TrackCreatorWindow : EditorWindow
         DoOverArc(radius, angleInRadians, true, (position, t, segmentLength) =>
         {
             var wall = GenerateWall(arc.transform);
-            wall.transform.localScale = new Vector3(1, 1, (radius + 0.6f) * segmentLength);
+            wall.transform.localScale = new Vector3(this.wallThickness, 1, (radius + 0.6f) * segmentLength);
             wall.transform.position = position;
             wall.transform.rotation = Quaternion.Euler(0, -t * Mathf.Rad2Deg + 180, 0);
             return true;
@@ -100,7 +102,7 @@ public class TrackCreatorWindow : EditorWindow
         return arc;
     }
 
-    private static GameObject GenerateGround(IEnumerable<Vector3> innerVertices, IEnumerable<Vector3> outerVertices, Transform parent)
+    private GameObject GenerateGround(IEnumerable<Vector3> innerVertices, IEnumerable<Vector3> outerVertices, Transform parent)
     {
         var trackGround = new GameObject("Ground", typeof(MeshRenderer), typeof(MeshFilter));
         var groundMeshFilter = trackGround.GetComponent<MeshFilter>();
@@ -111,7 +113,7 @@ public class TrackCreatorWindow : EditorWindow
         return trackGround;
     }
 
-    private static GameObject GenerateWall(Transform parent)
+    private GameObject GenerateWall(Transform parent)
     {
         var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         cube.name = "Wall";
@@ -124,7 +126,7 @@ public class TrackCreatorWindow : EditorWindow
     /// <summary>
     /// Generates ground mesh for a section of track. Expects innerVertices.length == outerVertices.length
     /// </summary>
-    private static Mesh GenerateGroundMesh(IEnumerable<Vector3> innerVertices, IEnumerable<Vector3> outerVertices)
+    private Mesh GenerateGroundMesh(IEnumerable<Vector3> innerVertices, IEnumerable<Vector3> outerVertices)
     {
         var groundMesh = new Mesh();
         groundMesh.vertices = innerVertices.Concat(outerVertices).ToArray();
