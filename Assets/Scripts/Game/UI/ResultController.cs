4b8d54b8df4074bd8a4dcd8c639b30372a37df86
Tom Hyde <thyde@neworbit.co.uk>
2020-02-06 21:45:53 +0000

    Nicely formatted results

---
+++ Assets/Scripts/Game/UI/ResultController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+class ResultController : MonoBehaviour
+{
+    public VehicleInfo Vehicle { get; set; }
+
+    [SerializeField]
+    private Text positionText;
+    [SerializeField]
+    private Text nameText;
+
+    private void Update()
+    {
+        this.positionText.text = NumberUtils.GetOrdinal(this.Vehicle.Position);
+        this.positionText.color = this.Vehicle.Color;
+        this.nameText.text = this.Vehicle.Driver.Name;
+        this.nameText.color = this.Vehicle.Color;
+    }
+}
--- Assets/Scripts/Game/UI/ResultsController.cs
+++ Assets/Scripts/Game/UI/ResultsController.cs
@@ -1,28 +1,45 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ResultsController : MonoBehaviour
 {
     private RaceController raceController;
-    private Text text;
+    [SerializeField]
+    private VerticalLayoutGroup layout;
+    [SerializeField]
+    private GameObject result;
+    private RaceStatus lastStatus;
+    private List<ResultController> resultControllers;
 
     private void Start()
     {
         this.raceController = FindObjectOfType<RaceController>();
-        this.text = this.GetComponent<Text>();
     }
 
     private void Update()
     {
+        if (this.lastStatus != RaceStatus.Finished && this.raceController.Status == RaceStatus.Finished)
+        {
+            this.resultControllers = new List<ResultController>();
+            foreach (var vehicle in this.raceController.Vehicles)
+            {
+                var result = Instantiate(this.result, this.layout.transform);
+                var resultController = result.GetComponent<ResultController>();
+                this.resultControllers.Add(resultController);
+            }
+        }
+
         if (this.raceController.Status == RaceStatus.Finished)
         {
-            this.text.enabled = true;
-            this.text.text = string.Join("\n", this.raceController.Vehicles.OrderBy(v => v.Position).Select(v => $"{NumberUtils.GetOrdinal(v.Position)}\t{v.Driver.Name}"));
+            var orderedVehicles = this.raceController.Vehicles.OrderBy(v => v.Position);
+            for(var i = 0; i < orderedVehicles.Count(); i++)
+            {
+                this.resultControllers[i].Vehicle = orderedVehicles.ElementAt(i);
+            }
         }
-        else
-        {
-            this.text.enabled = false;
-        }
+
+        this.lastStatus = this.raceController.Status;
     }
 }
